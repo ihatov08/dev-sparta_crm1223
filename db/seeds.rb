393f0a19d630b4c21a4b37f35b345fc86c368e47
tomoya <ihatov08@gmail.com>
2015-12-24 23:29:40 +0900

    indexの実装（seedファイルtimesメソッド)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+100.times do |index|
+  Customer.create( family_name: "スパルタ", given_name: "八幡平#{index}", email: "sparta#{index}@sparta.com")
+end
