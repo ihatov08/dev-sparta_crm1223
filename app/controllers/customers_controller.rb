ab802d53ff1c68afe239ebdbd6984f14a7936a13
tomoya <ihatov08@gmail.com>
2015-12-25 19:49:44 +0900

    new/createの実装ストロングパラメータ、formとrender,col-sm-offset-2

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,9 +4,13 @@ class CustomersController < ApplicationController
   end
 
   def new
+    @customer = Customer.new
   end
 
   def create
+    @customer = Customer.new(customer_params)
+    @customer.save
+    redirect_to @customer
   end
 
   def edit
@@ -20,5 +24,14 @@ class CustomersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(
+      :family_name,
+      :given_name,
+      :email
+      )
   end
 end
