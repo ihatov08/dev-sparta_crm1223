972aa0d19d1d56b378cc0a132c716f5d0937d717
tomoya <ihatov08@gmail.com>
2015-12-29 16:59:54 +0900

    commentモデルの開発（コメントフォーム）

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -39,6 +39,7 @@ end
 
   def show
     # @customer = Customer.find(params[:id])
+    @comment = Comment.new
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :customers
+  resources :comments, only: [:create]
   root 'customers#index'
   # get 'customers/index'
 
